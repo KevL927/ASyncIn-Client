36dfc4f1e0f8845985e508d6cb8792398d22be75
Michelle Nguyen <dr.mylann@gmail.com>
2016-12-02 00:05:52 -0600

    changed components to containers for SongSearch.js

---
+++ src/js/components/SongSearchPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../App.css';
+import {Link} from 'react-router';
+import SongSearch from '../containers/SongSearch'
+
+class SongSearchPage extends Component {
+  render() {
+    return (
+      <div className="musicPlayer">
+        <div className="musicPlayer-container">
+          <SongSearch/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SongSearchPage;
--- src/js/containers/SongSearch.js
+++ src/js/containers/SongSearch.js
@@ -41,4 +41,5 @@ const mapDispatchToProps = (dispatch) => {
     }
   };
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(SongSearch) ;
