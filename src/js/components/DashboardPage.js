671a1068e173bcba18b06b0a2e66a9e3b27e99fa
Michelle Nguyen <dr.mylann@gmail.com>
2016-12-02 00:36:01 -0600

    Made appropriate changes for components and containers

---
+++ src/js/components/DashboardPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../App.css';
+import Dashboard from '../containers/Dashboard'
+//import {Link} from 'react-router';
+
+class DashboardPage extends Component {
+  render() {
+    return (
+      <div className="DashboardPage">
+        <div className="DashboardPage-container">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DashboardPage;
---
+++ src/js/components/UserPlaylistsPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../../App.css';
+import UserPlaylists from '../containers/UserPlaylists';
+//import {Link} from 'react-router';
+
+class UserPlaylistsPage extends Component {
+  render() {
+    return (
+      <div className="UserPlaylistsPage">
+        <div className="UserPlaylistsPage-container">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserPlaylistsPage;
