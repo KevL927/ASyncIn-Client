4899cffe6a165c5263e199e36a1234d1190d3715
LavieMaru <laviemaru@gmail.com>
2016-12-03 19:52:25 -0600

    Add getUserPlaylists in action

--- src/js/containers/TestAction.js
+++ src/js/containers/TestAction.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import * as actions from '../actions/actions';
 // import {Link} from 'react-router';
 // import MusicPlayer from './MusicPlayer';
 
 class TestAction extends Component {
- 
-  render() {
-   return <h1>Test Actions</h1>
-}
+	onClickDispatch(event) {
+		event.preventDefault();
+		this.props.dispatch(actions.getUserPlaylists('iqz0zrbwsg40sg4ss8co44gww4o8gsg8os'))
+	}
+	render() {
+		console.log(this.props);
+	   return (
+	   	<div>
+	   		<h1>Test Actions</h1>
+	   		<h2>
+	   			My Playlists
+	   		</h2>
+	   		<button onClick={this.onClickDispatch.bind(this)} 		
+	>generate my playlists</button>
+	   	</div>
+	   );
+	}
 }
 
 const mapStateToProps = (state) => {
   return {
+    userSavedPlaylists: state.userSavedPlaylists
   }
 }
-const mapDispatchToProps = (dispatch) => {
-  return {
-  };
-};
 
-export default connect(mapStateToProps, mapDispatchToProps)(TestAction) ;
+export default connect(mapStateToProps)(TestAction) ;
