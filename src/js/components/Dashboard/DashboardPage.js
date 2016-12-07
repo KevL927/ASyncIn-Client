a1866362ece4a29bfb6f5319e0b07af9458570d1
surbhi <surbhi.poswalia@gmail.com>
2016-12-07 04:17:21 +0000

    align the dashboard page and route, reducers,actions for current user

--- src/js/components/Dashboard/DashboardPage.js
+++ src/js/components/Dashboard/DashboardPage.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
-import Dashboard from '../containers/Dashboard'
-import SongSearch from '../containers/SongSearch';
-import AddPlaylist from '../containers/AddPlaylist';
-import FavouritePlaylist from '../FavouritePlaylist/FavouritePlaylist'
+import {connect} from 'react-redux';
+import * as actions from '../../actions/actions';
+import SongSearch from '../SearchMusic/SongSearch';
+import AddPlaylist from '../AddPlaylist/AddPlaylist';
+import FavouritePlaylist from '../FavouritePlaylist/FavouritePlaylist';
+
 
 class DashboardPage extends Component {
+  
+  componentWillMount(){
+    //token, accesstoken
+    this.props.dispatch(actions.getCurrentUser('admin@email.com', 'iqz0zrbwsg40sg4ss8co44gww4o8gsg8os'));
+  }
+  
+  renderComponents() {
+    console.log(this.props)
+    if(this.props.currentUser) {
+      return (
+        <div>
+          <h3>Welcome, {this.props.currentUser.username}</h3>
+          <SongSearch />
+          <FavouritePlaylist favouritePlaylists={this.props.currentUser.favouritePlaylists} />
+          <AddPlaylist currentUser={this.props.currentUser} userSavedPlaylists={this.props.userSavedPlaylists}/>
+        </div>
+      );
+    }else{
+      return <div>Loading</div>;
+    }
+  }
+  
   render() {
     return (
       <div className="DashboardPage">
-        <div className="DashboardPage-container">
-          <SongSearch />
-          <AddPlaylist />
-          <UserSavedPlaylist />
-          <FavouritePlaylist />
-        </div>
+          {this.renderComponents()}
       </div>
     );
   }
+  
 }
 
-export default DashboardPage;
+
+export default connect(  
+  ({ currentUser, userSavedPlaylists }) => 
+  ({ currentUser, userSavedPlaylists })
+)(DashboardPage);
+
+    // <SongSearch />
+          // <AddPlaylist />
+          // <UserSavedPlaylist />
+          // <FavouritePlaylist favouritePlaylists={this.props.currentUser.favouritePlaylist} />
