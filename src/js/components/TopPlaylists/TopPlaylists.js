6343d7ecafe1c1ed8237a298e6b36358d0901b96
KL012 <kevl927@gmail.com>
2016-12-07 09:23:36 +0000

    Refactor TopPlaylist component and display top 4 playlist titles

--- src/js/components/TopPlaylists/TopPlaylists.js
+++ src/js/components/TopPlaylists/TopPlaylists.js
@@ -1,47 +1,33 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../../actions/actions';
+import Top3Playlists from './Top3Playlists';
+import Top4To10Playlists from './Top4To10Playlists';
 
 
-class TopPlaylist extends Component {
+class TopPlaylists extends Component {
  
     componentWillMount() {
         this.props.dispatch(actions.getTopPlaylist('iqz0zrbwsg40sg4ss8co44gww4o8gsg8os'));
     }
-    
-    renderTopPlaylist(){
-        let playlist = [];
-        if(!this.props.topPlaylists) {
-          playlist = <h2>No Top Playlists</h2>  
-        }
-        else{
-            // return <div>{this.props.topPlaylists[0].name}</div>
-            playlist = this.props.topPlaylists.map((playlist, index) => {
-                return (
-                  <li key={index}>
-                        {playlist.name}
-                  </li>
-                );
-            })
-        }
-     return playlist;
+    // <Top3Playlists playlistObject={this.props.topPlaylists.slice(0,3)} />,
+    renderToplists() {
+        if(this.props.topPlaylists) {
+            return (
+                
+                <Top4To10Playlists playlistArray={this.props.topPlaylists.slice(4,10)} />
+            );
+        } 
+        return <h2>Loading Playlist</h2>;
     }
     
-
 	render() {
-		return (
-            <div>
-                <div>{this.renderTopPlaylist()}</div>
-            </div>
-		);
+		return <div>{this.renderToplists()}</div>;
 	}
+	
 }
 
-// const mapStateToProps = (state) => {
-//   return {
-//     topPlaylists: state.topPlaylists,
-//     error: state.error
-//   }
-// }
 
-export default connect()(TopPlaylist);
+export default connect(
+    ({ topPlaylists }) => ({ topPlaylists })
+)(TopPlaylists);
