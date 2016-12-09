308eb8c9d3408336a463af485107df1f520f4b4e
LavieMaru <laviemaru@gmail.com>
2016-12-08 21:35:48 -0600

    Add this.props.currentUser to dispatch getTopPlaylist

--- src/js/components/Pages/TopPlaylistsPage.js
+++ src/js/components/Pages/TopPlaylistsPage.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../../actions/actions';
 import RenderPlaylist from '../TopPlaylists/RenderPlaylist';
-
+import update from 'react-addons-update';
 
 class TopPlaylistsPage extends Component {
  
+    state = {
+        isOpenedArray: []
+    }
+
+    onClickAddToQueue(playlist, event){
+        this.props.dispatch(actions.queue(playlist.tracks));  
+    }
+
+    expandCollapse(index, event) {
+        event.preventDefault();
+        if (this.state.isOpenedArray.indexOf(index) === -1) {
+            const tempOpenedArr = update(this.state.isOpenedArray, {$push: [index]});
+            this.setState({isOpenedArray: tempOpenedArr})
+        } else {
+            const index = this.state.isOpenedArray.indexOf(index)
+            const tempOpenedArr = update(this.state.isOpenedArray, {$splice: [[index, 1]]});
+            this.setState({isOpenedArray: tempOpenedArr});
+        }   
+    }
+
+    checkOpenedOrNot(index) {
+        if (this.state.isOpenedArray.indexOf(index) !== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     componentWillMount() {
         this.props.dispatch(actions.getTopPlaylist(this.props.currentUser.accessToken));
     }
