eb5aab6f6497550ecfed3daa9dca63dd7e379e99
LavieMaru <laviemaru@gmail.com>
2016-12-08 21:43:03 -0600

    Top playlist 3-10 has expand and collapse

--- src/js/components/TopPlaylists/RenderPlaylist.js
+++ src/js/components/TopPlaylists/RenderPlaylist.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import * as actions from '../../actions/actions';
 import RenderTracks from '../PlaylistPlayer/RenderTracks';
 import playMusicFunc from '../MusicPlayer/playMusicFunc';
+import Collapse from 'react-collapse';
+import update from 'react-addons-update';
 
 class RenderPlaylist extends Component {
-  
+    state = {
+      isOpenedArray: []
+    }
+
+    onClickAddToQueue(playlist, event){
+        this.props.dispatch(actions.queue(playlist.tracks));  
+    }
+
+    expandCollapse(index, event) {
+      event.preventDefault();
+      if (this.state.isOpenedArray.indexOf(index) === -1) {
+        const tempOpenedArr = update(this.state.isOpenedArray, {$push: [index]});
+            this.setState({isOpenedArray: tempOpenedArr})
+      } else {
+        const index = this.state.isOpenedArray.indexOf(index)
+        const tempOpenedArr = update(this.state.isOpenedArray, {$splice: [[index, 1]]});
+        this.setState({isOpenedArray: tempOpenedArr});
+      }
+
+      
+    }
+
+    checkOpenedOrNot(index) {
+      if (this.state.isOpenedArray.indexOf(index) !== -1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    viewTracks(playlist) {
+      if(playlist) {
+        return <ul><RenderTracks playlistObject={playlist} /></ul>
+      }
+      return;
+    }
+
   rendertheStuff() {
     if(this.props.playlistArray.length < 4) {
       return (
@@ -27,8 +66,15 @@ class RenderPlaylist extends Component {
           <h2>Top 4-10 Playlists</h2>
           {this.props.playlistArray.map((playlist, index) => (
             <div>
-              <li>#{index+4} - {playlist.name}</li>
-              <li>favourites: {playlist.rating}</li>
+              <li onClick={this.expandCollapse.bind(this, index)} ref={index}>
+                #{index+4} - {playlist.name}
+              </li>
+              <li>
+                favourites: {playlist.rating}
+              </li>
+              <Collapse isOpened={this.checkOpenedOrNot(index)}>
+              {this.viewTracks(playlist)}
+             </Collapse>
             </div>
           ))}
         </div>
