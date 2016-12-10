a5180f9ed25fc5ae59512808989a69ae293cf4aa
surbhi <surbhi.poswalia@gmail.com>
2016-12-10 19:32:30 +0000

    add scroll bar to dashboard user saved playlist

--- src/js/components/UserPlaylists/MyPlaylistsDashboard.js
+++ src/js/components/UserPlaylists/MyPlaylistsDashboard.js
@@ -4,6 +4,7 @@ import * as actions from '../../actions/actions';
 import RenderSavedPlaylistTracks from '../PlaylistPlayer/RenderSavedPlaylistTracks';
 import Collapse from 'react-collapse';
 import update from 'react-addons-update';
+import ScrollArea from 'react-scrollbar';
 
 class MyPlaylistsDashboard extends Component {
 	state = {
@@ -56,9 +57,10 @@ class MyPlaylistsDashboard extends Component {
 		
 	}
 	edit(playlistObject, event){
+		console.log(playlistObject);
 		const updatedPlaylist = update(playlistObject, {name: {$set:this.refs.input.value}})
-		this.props.dispatch(actions.updatePlaylist(updatedPlaylist, this.props.currentUser.accessToken));
-		this.setState({
+	this.props.dispatch(actions.updatePlaylist(updatedPlaylist, this.props.currentUser.accessToken));
+	this.setState({
 			editable: null
 		})
 	}
@@ -74,7 +76,7 @@ class MyPlaylistsDashboard extends Component {
 	        	 <button onClick={this.onClickAddToQueue.bind(this, playlist)}>Add to Queue</button>
 	        	 <button onClick={this.deletePlaylist.bind(this, playlist)}>Delete Playlist</button>
 	        	 <button onClick={this.editPlaylistName.bind(this, playlist)}>Edit</button>
-	        	 {this.state.editable === playlist._id ?<input contentEditable onBlur={this.edit.bind(this, playlist)} ref="input" />:<h4 onClick={this.expandCollapse.bind(this, index)} ref={index}>{playlist.name}</h4> }
+	        	 {this.state.editable == playlist._id ?<input contentEditable onBlur={this.edit.bind(this, playlist)} ref="input" />:<h4 onClick={this.expandCollapse.bind(this, index)} ref={index}>{playlist.name}</h4> }
 	          	 
 		         <Collapse isOpened={this.checkOpenedOrNot(index)}>
 		         	{this.viewTracks(playlist)}
@@ -92,13 +94,16 @@ class MyPlaylistsDashboard extends Component {
 	}
 
 	render() {
+		{	console.log(this.state.editable)}
 		return (
 			<div className="UserPlaylist">
+			<h2>My Saved Playlists</h2>
+			<ScrollArea speed={0.8} className="area" contentClassName="content" horizontal={false} >
 			<div className="UserPlaylist-container">
-				<h2>My Saved Playlists</h2>
 		     	{this.generateResult(this.props.userSavedPlaylists)}
 		     	{this.updateServerQueue()}
 			</div>
+			</ScrollArea>
 			</div>
 		);
 	}
