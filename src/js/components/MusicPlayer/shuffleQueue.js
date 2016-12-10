70eb28cff14502105ebaff6b68da26d56ca6e70f
KL012 <kevl927@gmail.com>
2016-12-10 22:24:56 +0000

    Add shuffle functionality on the music player

---
+++ src/js/components/MusicPlayer/shuffleQueue.js
@@ -0,0 +1,18 @@
+//The de-facto unbiased shuffle algorithm is the Fisher-Yates (aka Knuth) Shuffle.
+//See https://github.com/coolaj86/knuth-shuffle
+
+export default (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  
+  return array;
+};
