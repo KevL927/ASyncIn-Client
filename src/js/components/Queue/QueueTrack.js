baad362bd363e24acfdc4ced105a621da93c8ac8
LavieMaru <laviemaru@gmail.com>
2016-12-12 16:06:50 -0600

    Add to playlist function works

--- src/js/components/Queue/QueueTrack.js
+++ src/js/components/Queue/QueueTrack.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import AddPlaylist from '../AddPlaylist/AddPlaylist';
 
-export default ({ onTrackItemClick, track, onClickDeleteQueueTrack, trackIndex, onTrackPlayNow }) => {
-  
+export default ({ currentUser, userSavedPlaylists, error, feedback, track,  onTrackItemClick, onClickDeleteQueueTrack, trackIndex, onTrackPlayNow }) => {
   return (
     <li className="track">
     	<button onClick={(event) => onClickDeleteQueueTrack(event, trackIndex)}>Delete Track</button>
     	<button onClick={(event) => onTrackItemClick(event, track)}>Add To Queue</button>
     	<a onClick={(event) => onTrackPlayNow(event, track.link)} >{track.title}</a>
+		<AddPlaylist error={error} feedback={feedback} currentUser={currentUser} userSavedPlaylists={userSavedPlaylists} newPlaylist={[track]} />
     </li>
   );
         
