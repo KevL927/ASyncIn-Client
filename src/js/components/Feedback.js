9f5be6668d40e633f721716264b8e2897ba27946
KL012 <kevl927@gmail.com>
2016-12-12 21:37:02 +0000

    Refactor Feedback component to stateless

--- src/js/components/Feedback.js
+++ src/js/components/Feedback.js
@@ -1,14 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 
-class Feedback extends Component {
-  render () {
-    return ( 
-    	<div>
-    		  <p>{this.props.feedback}</p>
-    	</div>
-    )
-  }
-}
-
-export default Feedback;
+export default ({ feedback }) => {
+    return <div>{ feedback }</div>;
+};
