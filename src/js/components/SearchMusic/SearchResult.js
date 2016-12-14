39f6cde9bad5ea5704582628eac8ac381920cd08
LavieMaru <laviemaru@gmail.com>
2016-12-14 03:08:26 -0600

    Clean up unnecessary code

--- src/js/components/SearchMusic/SearchResult.js
+++ src/js/components/SearchMusic/SearchResult.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Thumbnail from 'react-bootstrap'
 
 export default (props) => {
   return (
       <div className="search-result">
 
-      <img className="thumbnail" src={props.track.thumbnail}/>
+      <img className="thumbnail" src={props.track.thumbnail} alt="thumbnail"/>
       	<div>{props.track.title}</div>
       	
       </div>
--- src/js/components/TopPlaylists/RenderPlaylist.js
+++ src/js/components/TopPlaylists/RenderPlaylist.js
@@ -38,13 +38,13 @@ class RenderPlaylist extends Component {
       )
     }
 
-    expandCollapse(index, event) {
+    expandCollapse(arrIndex, event) {
       event.preventDefault();
-      if (this.state.isOpenedArray.indexOf(index) === -1) {
-          const tempOpenedArr = update(this.state.isOpenedArray, {$push: [index]});
+      if (this.state.isOpenedArray.indexOf(arrIndex) === -1) {
+          const tempOpenedArr = update(this.state.isOpenedArray, {$push: [arrIndex]});
           this.setState({isOpenedArray: tempOpenedArr})
       } else {
-          const index = this.state.isOpenedArray.indexOf(index)
+          let index = this.state.isOpenedArray.indexOf(arrIndex);
           const tempOpenedArr = update(this.state.isOpenedArray, {$splice: [[index, 1]]});
           this.setState({isOpenedArray: tempOpenedArr});
       }
