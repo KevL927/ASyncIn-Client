385d085e13f753f2c668285d37f1954e31e0321f
surbhi <surbhi.poswalia@gmail.com>
2016-12-14 22:57:45 +0000

    add icons to Queue

--- src/js/components/Queue/QueueTrack.js
+++ src/js/components/Queue/QueueTrack.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import FaTrash from 'react-icons/lib/fa/trash';
+import TiPlus from 'react-icons/lib/ti/plus';
+import FaArrowCircleUp from 'react-icons/lib/fa/arrow-circle-up';
+import FaArrowCircleDown from 'react-icons/lib/fa/arrow-circle-down';
 
 export default ({ currentUser, userSavedPlaylists, error, feedback, track,  onTrackItemClick, onClickDeleteQueueTrack, moveTrackInQueue, trackIndex, onTrackPlayNow }) => {
   return (
     <li className="track">
-    	<button onClick={(event) => onClickDeleteQueueTrack(event, trackIndex)}>Delete Track</button>
-    	<button onClick={(event) => onTrackItemClick(event, track)}>Add To Queue</button>
-    	<button onClick={(event) => moveTrackInQueue(event, trackIndex, 'up')}>Move up</button>
-    	<button onClick={(event) => moveTrackInQueue(event, trackIndex, 'down')}>Move down</button>
+    	<button onClick={(event) => onClickDeleteQueueTrack(event, trackIndex)}><FaTrash size={22} /></button>
+    	<button onClick={(event) => onTrackItemClick(event, track)}><TiPlus size={22} /></button>
+    	<button onClick={(event) => moveTrackInQueue(event, trackIndex, 'up')}><FaArrowCircleUp size={22} /></button>
+    	<button onClick={(event) => moveTrackInQueue(event, trackIndex, 'down')}><FaArrowCircleDown size={22} /></button>
     	<div onClick={(event) => onTrackPlayNow(event, track.link)} id="Q_Track_List">{track.title}</div>
     </li>
   );
