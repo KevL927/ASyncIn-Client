a5665979fedef66677112f9ac4314fdccae0aa82
KL012 <kevl927@gmail.com>
2016-12-16 03:10:40 +0000

    Fix login and register page div size and center text in textbox it

--- src/js/components/Pages/ContactsPage.js
+++ src/js/components/Pages/ContactsPage.js
@@ -7,7 +7,18 @@ class ContactDev extends Component {
     return (
       <div className="contactDev">
         <div className="contactDevContainer">
-          
+          <h1>Surbhi Poswali</h1>
+            <h3>Project Manager</h3>
+              <p>Hey, there. I'm Surbhi. I'm a famous Bollywood star, but that life was really boring so I took on becoming a Web Developer. Contact me!</p>
+          <h1>Lavie Ruan</h1>
+            <h3>Scrum Master</h3>
+              <p>Hey, there. I'm Lavie. I like to eat organic things, especailly when I dress up like a panda. Contact me!</p>
+          <h1>Kevin Lee</h1>
+            <h3>Product Manager</h3>
+              <p>Hey, there. I'm Kevin. I am a korean pop star singing rap and joining hardcore Korean gangs, but I left that rigid life behind to do something more stable and safe. Contact me!</p>
+          <h1>Michelle Nguyen</h1>
+            <h3>Design Manager</h3>
+              <p>Hey. Contact me. kthxbai</p>
         </div>
       </div>
     );
