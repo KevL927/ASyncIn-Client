3da774b2d77bfba813e342e8c22d0bdb1407b363
Lavie Ruan <lavieruan@Lavies-MacBook.local>
2017-01-31 16:11:50 -0600

    edit account settings box

--- src/js/components/AccountSettings/AccountSettingsPage.js
+++ src/js/components/AccountSettings/AccountSettingsPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import AccountSettings from '../AccountSettings/AccountSettings';
+import './styles.css';
 
 class AccountSettingsPage extends Component {
   render() {
