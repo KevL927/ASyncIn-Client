5bcc6ec1b3b0f43269f2776b3fe3f556806f7537
KL012 <kevl927@gmail.com>
2017-02-03 22:47:58 +0000

    Add semicolon and delete unnecessary spaces

--- src/js/components/Feedback.js
+++ src/js/components/Feedback.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 export default ({ feedback }) => {
     return <span className="feedback">{ feedback }</span>;
 };
--- src/js/components/Loading.js
+++ src/js/components/Loading.js
@@ -2,5 +2,5 @@ import React from 'react';
 import Loading from 'react-loading';
  
 export default () => {
-    return <div><Loading type='spokes' color='#e3e3e3' /></div>
+    return <div><Loading type='spokes' color='#e3e3e3' /></div>;
 };
